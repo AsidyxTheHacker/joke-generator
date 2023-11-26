cea6b086285f8bb2f1b8cb80c817459327b4334c
Adam <adammiller.2000nj@gmail.com>
2023-11-26 14:39:26 -0500

    changed api settings and added fade animation to p element

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 const joke = document.getElementById('joke');
 const btn = document.getElementById('btn');
-const url = "https://v2.jokeapi.dev/joke/Any?type=single";
+const url = "https://v2.jokeapi.dev/joke/Miscellaneous,Dark,Pun,Spooky,Christmas?type=single";
 
 let getJoke = () => {
+    joke.classList.remove('fade')
     fetch(url)
     .then(data => data.json())
     .then(item => {
         joke.textContent = `${item.joke}`;
+        joke.classList.add('fade')
     })
 };
 
 btn.addEventListener('click', getJoke);
-
 getJoke();
